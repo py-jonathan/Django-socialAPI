a0fabc6ea50a798cd0f3d2137906493b49522a41
py-jonathan <pyjonpk@gmail.com>
2024-07-04 17:19:52 +0300

    Create basic User models and authentications with Django REST;

---
+++ core/routers.py
@@ -0,0 +1,21 @@
+from rest_framework import routers
+from core.user.viewsets import UserViewSet
+from core.auth.viewsets import RegisterViewSet
+from core.auth.viewsets import LoginViewSet
+from core.auth.viewsets import RefreshViewSet
+
+router = routers.SimpleRouter()
+
+# ################### AUTH ###################### #
+router.register(r"auth/register", RegisterViewSet, basename="auth-register")
+router.register(r"auth/login", LoginViewSet, basename="auth-login")
+router.register(r"auth/refresh", RefreshViewSet, basename="auth-refresh")
+
+# ############################################################
+######### USER ######################
+# ############################################################
+
+router.register(r"user", UserViewSet, basename="user")
+urlpatterns = [
+    *router.urls,
+]
